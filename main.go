65e0d520bc9a96cafec7904ac80562fec7709d24
Antuan01 <joseitox_4@hotmail.com>
2021-03-29 22:10:50 -0400

    first template

--- main.go
+++ main.go
@@ -8,9 +8,16 @@ import (
 func main() {
   r := gin.Default()
 
+  r.LoadHTMLGlob("templates/*")
+
   r.GET("/", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{"data": "hello world"})    
   })
 
+  r.GET("/index", func(c *gin.Context) {
+   c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})   
+  })
   r.Run()
 }
